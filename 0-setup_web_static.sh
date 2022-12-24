9b76b20deee8990cd3cd177c124ef4426dea6170
callmeoli <olikorma@gmail.com>
2022-12-24 16:14:54 +0300

    hot_fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 # bash script that facilitate deployment of webstatic
 
-if ! command -v <the_command> &> /dev/null
+if ! command -v nginx &> /dev/null
 then
     apt-get upgrade
     apt-get install nginx 
 fi
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-
-touch /data/web_static/releases/test/index.html
-echo "Test for index" >> index.html
-
-ln -sf /data/web_static/current /data/web_static/releases/test/
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "This is for testing" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 new_loc="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
 sed -i "38i\ $new_loc" /etc/nginx/sites-available/default
-service restart nginx
+
+sudo /etc/init.d/nginx restart
