d445fde8ede2aace20b4bf7dcfc78baaad89ab94
callmeoli <olikorma@gmail.com>
2022-12-25 15:36:03 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,20 +2,25 @@
 """ Function that compress a folder """
 from fabric.api import env, run, put
 import os
+env.hosts = ['34.232.69.130', '100.26.216.95']
+
 
 def do_deploy(archive_path):
     """ a function that deploy our web static"""
-    env.hosts = ['34.232.69.130', '100.26.216.95']
+
     if os.path.exists(archive_path):
         try:
             put(archive_path, "/tmp/")
             os.path.basename(archive_path)
             file = os.path.basename(archive_path)
             filename = file.split(".")[0]
-            run("tar –xvzf /tmp/{} –C /data/web_static/releases/{}".format(file, filename))
+            run("mkdir -p /data/web_static/releases/{}/".format(filename))
+            run("tar -xzf /tmp/{0} -C /data/web_static/\
+                releases/{1}/".format(file, filename))
             run("rm -rf /tmp/{}".format(archive_path))
             run("unlink /data/web_static/current")
-            run("ln -s /data/web_static/current /data/web_static/releases/{}".format(filename))
+            run("ln -s /data/web_static/current /data/\
+                web_static/releases/{}".format(filename))
         except Exception:
             return False
     else:
