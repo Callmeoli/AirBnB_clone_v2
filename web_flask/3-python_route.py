efdc86e456f326859695840a97d13f5e9ffa8efc
allmeoli <olikorma@gmail.com>
2023-02-10 19:13:27 +0300

    hot_fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
-""" script that start flask web app"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """return hello"""
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def Hbnb():
-    """ return HBNB"""
+def hbnb():
+    """returns HBNB"""
     return 'HBNB'
 
 
-@app.route('/c/<string:text>', strict_slashes=False)
-def C(text):
-    """ return C followed by the value of text"""
-    return "C {}".format(text.replace("_", " "))
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/<string:text>', strict_slashes=False)
-def Python(text="Python"):
-    """ return C followed by the value of text"""
-    return "Python {}".format(text.replace("_", " "))
-
-
-@app.route('/python')
-@app.route('/python/<string:text>')
-def python_route(text='is cool'):
-    # displays "Python + text"
-    return "Python {}".format(text.replace("_", " "))
-
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python3
 """
-    Script that starts a Flask web application
-    /: display “Hello HBNB!”
-    /hbnb: display “HBNB”
-    /c/<text>: display “C ” followed by the value of the
-    text variable (replace underscore _ symbols with a space)
-    /python/(<text>): display “Python ”, followed by the value of the
-    text variable (replace underscore _ symbols with a space)
-    the default value of text is “is cool”
-    /number/<n>: display “n is a number” only if n is an integer
+starts a Flask web application
 """
 
 from flask import Flask
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello():
-    # displays “Hello HBNB!”
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    # displays "HBNB"
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 
-@app.route('/c/<string:text>')
-def c_route(text):
-    # displays "C + text"
-    return "C {}".format(text.replace("_", " "))
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python')
-@app.route('/python/<string:text>')
-def python_route(text='is cool'):
-    # displays "Python + text"
-    return "Python {}".format(text.replace("_", " "))
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>')
-def number_route(n):
-    # display “n is a number” only if n is an integer
-    return '{} is a number'.format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
